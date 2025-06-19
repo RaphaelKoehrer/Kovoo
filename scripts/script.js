b5276e05c99737e0e85f671bc854d530a7d58249
Raphael Koehrer <r.koehrer@students.htl-leonding.ac.at>
2025-06-19 22:10:42 +0200

    impressum

--- scripts/script.js
+++ scripts/script.js
@@ -82,7 +82,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const navIcon = document.getElementById('navIcon');
 
     navIcon.addEventListener('click', () => {
-            location.href = '../index.html';
+        
+            location.href = '/index.html';
 
     });
     
